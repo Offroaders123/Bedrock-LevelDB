90b31b0a3ba70e9cf5ba7b5efd19a4d50cca0eb8
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-01-29 01:36:35 -0800

    Database Key Test
    Tried to read all of the keys from the database, using the iterator syntax.
    
    Found some docs that will help me out with parsing the data inside of the database.
    
    https://minecraft.fandom.com/wiki/Bedrock_Edition_level_format
    https://wiki.vg/Bedrock_Edition_level_format
    
    Interesting note/realization/question:
    "So, sounds like Regions aren't a thing in Bedrock, interestingly enough?"

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,12 @@
 import * as fs from "node:fs/promises";
 import { LevelDB } from "leveldb-zlib";
+
+const db = new LevelDB("../test/My World/db/");
+
+await db.open();
+
+const iterator = db.getIterator();
+
+for await (const key of iterator){
+  console.log(key);
+}
