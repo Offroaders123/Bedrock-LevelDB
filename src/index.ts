a6ad426aa50afbf17e7c69de529ee35216ee26f9
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-01-29 02:00:10 -0800

    NBT Key Try Catch
    Not sure of the specific key entry formats yet, so I just ran a try / catch over all of the keys in the database, and tried parsing each as NBT, just to see what happens.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import * as fs from "node:fs/promises";
 import { LevelDB } from "leveldb-zlib";
+import * as NBT from "nbtify";
 
 const db = new LevelDB("../test/My World/db/");
 
 await db.open();
 
 for await (const key of db){
-  console.log((key as Buffer[]).map(buffer => buffer.toString()),"\n");
+  for (const thing of key){
+    try {
+      const nbt = await NBT.read(thing,{
+        endian: "little",
+        compression: null,
+        isNamed: true,
+        isBedrockLevel: false
+      });
+      console.log(nbt);
+    } catch {}
+  }
 }
 
 await db.close();
