40acaed0bda97a4e156c56dc5a3b7925e73cde7f
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-02-06 14:06:45 -0800

    Key Type Mapping
    Looking at the mapped names for each database entry type.

--- src/index.ts
+++ src/index.ts
@@ -2,61 +2,34 @@ import * as fs from "node:fs/promises";
 import type { LevelDB } from "leveldb-zlib";
 import * as NBT from "nbtify";
 
-export type KEY = typeof KEY[keyof typeof KEY];
+export type KEY = keyof typeof KEY;
 
 export const KEY = {
-  Data3D: 43,
-  Version: 44,
-  Data2D: 45,
-  Data2DLegacy: 46,
-  SubChunkPrefix: 47,
-  LegacyTerrain: 48,
-  BlockEntity: 49,
-  Entity: 50,
-  PendingTicks: 51,
-  LegacyBlockExtraData: 52,
-  BiomeState: 53,
-  FinalizedState: 54,
-  ConversionData: 55,
-  BorderBlocks: 56,
-  HardcodedSpawners: 57,
-  RandomTicks: 58,
-  CheckSums: 59,
-  GenerationSeed: 60,
-  GeneratedPreCavesAndCliffsBlending: 61,
-  BlendingBiomeHeight: 62,
-  MetaDataHash: 63,
-  BlendingData: 64,
-  ActorDigestVersion: 65,
-  LegacyVersion: 118
-} as const satisfies typeof LevelChunkTag;
-
-enum LevelChunkTag {
-  Data3D = 43,
-  Version, // This was moved to the front as needed for the extended heights feature. Old chunks will not have this data.
-  Data2D,
-  Data2DLegacy,
-  SubChunkPrefix,
-  LegacyTerrain,
-  BlockEntity,
-  Entity,
-  PendingTicks,
-  LegacyBlockExtraData,
-  BiomeState,
-  FinalizedState,
-  ConversionData, // data that the converter provides, that are used at runtime for things like blending
-  BorderBlocks,
-  HardcodedSpawners,
-  RandomTicks,
-  CheckSums,
-  GenerationSeed,
-  GeneratedPreCavesAndCliffsBlending = 61, // not used, DON'T REMOVE
-  BlendingBiomeHeight = 62, // not used, DON'T REMOVE
-  MetaDataHash,
-  BlendingData,
-  ActorDigestVersion,
-  LegacyVersion = 118,
-}
+  43: "Data3D",
+  44: "Version",
+  45: "Data2D",
+  46: "Data2DLegacy",
+  47: "SubChunkPrefix",
+  48: "LegacyTerrain",
+  49: "BlockEntity",
+  50: "Entity",
+  51: "PendingTicks",
+  52: "LegacyBlockExtraData",
+  53: "BiomeState",
+  54: "FinalizedState",
+  55: "ConversionData",
+  56: "BorderBlocks",
+  57: "HardcodedSpawners",
+  58: "RandomTicks",
+  59: "CheckSums",
+  60: "GenerationSeed",
+  61: "GeneratedPreCavesAndCliffsBlending",
+  62: "BlendingBiomeHeight",
+  63: "MetaDataHash",
+  64: "BlendingData",
+  65: "ActorDigestVersion",
+  118: "LegacyVersion"
+} as const;
 
 export async function read(db: LevelDB) {
   const result: Record<string,any> = {};
@@ -79,7 +52,7 @@ export async function read(db: LevelDB) {
       const y = view.getInt32(3,true);
       const type = view.getUint8(8) as KEY;
       const index = (type === 47) ? view.getUint8(9) : "";
-      console.log(x,y,type,index);
+      console.log(x,y,KEY[type],index);
     }
   }
 
