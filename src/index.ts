d50235af336f282f1dc5019bc519c49928b54b7e
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-02-06 13:05:57 -0800

    Modern ActorStorage Key IDs
    Updated the key entries for the ActorStorage types in my `KEY` object. The article on the Microsoft Creator page helped out a ton, big thanks to that one! It layed out a bunch of missing ID indices that weren't described on the Minecraft Wiki.
    
    https://learn.microsoft.com/en-us/minecraft/creator/documents/actorstorage#non-actor-data-chunk-key-ids

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ export type KEY = typeof KEY[keyof typeof KEY];
 
 export const KEY = {
   Data3D: 43,
-  ChunkVersion: 44,
+  Version: 44,
   Data2D: 45,
   Data2DLegacy: 46,
   SubChunkPrefix: 47,
@@ -14,15 +14,49 @@ export const KEY = {
   BlockEntity: 49,
   Entity: 50,
   PendingTicks: 51,
-  BlockExtraData: 52,
+  LegacyBlockExtraData: 52,
   BiomeState: 53,
   FinalizedState: 54,
+  ConversionData: 55,
   BorderBlocks: 56,
-  HardCodedSpawnAreas: 57,
+  HardcodedSpawners: 57,
   RandomTicks: 58,
-  Checksums: 59,
-  LegacyChunkVersion: 118
-} as const;
+  CheckSums: 59,
+  GenerationSeed: 60,
+  GeneratedPreCavesAndCliffsBlending: 61,
+  BlendingBiomeHeight: 62,
+  MetaDataHash: 63,
+  BlendingData: 64,
+  ActorDigestVersion: 65,
+  LegacyVersion: 118
+} as const satisfies typeof LevelChunkTag;
+
+enum LevelChunkTag {
+  Data3D = 43,
+  Version, // This was moved to the front as needed for the extended heights feature. Old chunks will not have this data.
+  Data2D,
+  Data2DLegacy,
+  SubChunkPrefix,
+  LegacyTerrain,
+  BlockEntity,
+  Entity,
+  PendingTicks,
+  LegacyBlockExtraData,
+  BiomeState,
+  FinalizedState,
+  ConversionData, // data that the converter provides, that are used at runtime for things like blending
+  BorderBlocks,
+  HardcodedSpawners,
+  RandomTicks,
+  CheckSums,
+  GenerationSeed,
+  GeneratedPreCavesAndCliffsBlending = 61, // not used, DON'T REMOVE
+  BlendingBiomeHeight = 62, // not used, DON'T REMOVE
+  MetaDataHash,
+  BlendingData,
+  ActorDigestVersion,
+  LegacyVersion = 118,
+}
 
 export async function read(db: LevelDB) {
   const result: Record<string,any> = {};
