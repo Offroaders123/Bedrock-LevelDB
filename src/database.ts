096595ab9867cdf86e37795ecfd16e5c936be767
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-10-18 00:48:52 -0700

    Chunk Entries List
    https://en.wikipedia.org/wiki/Aja_(album)
    https://stackoverflow.com/questions/50376977/generic-type-to-get-enum-keys-as-union-string-in-typescript
    https://stackoverflow.com/questions/60260391/typescript-mapped-type-to-make-every-property-readonly-except-chosen-properties

--- src/database.ts
+++ src/database.ts
@@ -1,19 +1,49 @@
 import { LevelDB } from "leveldb-zlib";
+import { KEY } from "./key.js";
 
-export interface Entries {
-  [key: string]: Buffer;
+declare module "leveldb-zlib" {
+  export class LevelDB {
+    [Symbol.asyncIterator](): AsyncGenerator<[Buffer,Buffer],void,void>;
+  }
 }
+
+export type Chunk = {
+  -readonly [K in keyof typeof KEY]: Buffer;
+} & {
+  x: number;
+  y: number;
+};
+
+export type Entries = Chunk[];
 
 export async function readDatabase(path: string): Promise<Entries> {
   const db = new LevelDB(path);
   await db.open();
 
-  const entries: Entries = {};
+  const entries: Entries = [];
 
   for await (const [key,value] of db){
-    entries[(key as Buffer).toString("hex")] = value;
+    const { x, y, type } = readKey(key);
+    if (!entries.some(entry => entry.x === x && entry.y === y)){
+      entries.push({ x, y } as Chunk);
+    }
+    entries.find(entry => entry.x === x && entry.y === y)![type] = value;
   }
 
   await db.close();
   return entries;
+}
+
+export interface Key {
+  x: number;
+  y: number;
+  type: keyof typeof KEY;
+}
+
+export function readKey(key: Buffer): Key {
+  const view = new DataView(key.buffer,key.byteOffset,key.byteLength);
+  const x = view.getInt32(0,true);
+  const y = view.getInt32(3,true);
+  const type = KEY[view.getUint8(8) as KEY] as keyof typeof KEY;
+  return { x, y, type };
 }
