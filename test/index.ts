333e911fc39bd0a8d1fe787840a367997e88752f
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-12-13 13:53:49 -0800

    Block Entity Parsing Demo
    Working on trying to figure out parsing Actor Storage values, since they use specific formats, not always just plain NBT data.
    
    https://minecraft.wiki/w/Bedrock_Edition_level_format#:~:text=1-,BlockEntity,-List%20of%20NBT

--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,14 @@
 import { fileURLToPath } from "node:url";
+import { read } from "nbtify";
 import { readDatabase } from "../src/index.js";
 
 const WORLD = fileURLToPath(new URL("../test/My World/db",import.meta.url));
 
 const data = await readDatabase(WORLD);
-console.log(data);
+console.log(await Promise.all(
+  data.chunks
+    .map(chunk => chunk.BlockEntity)
+    .filter(blockEntity => blockEntity !== undefined)
+    // .map(blockEntity => blockEntity.join(" "))
+    .map(async blockEntity => (await read(blockEntity,{ endian: "little", strict: false })).data)
+));
