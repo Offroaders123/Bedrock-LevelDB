9408c26bc61de3756575358c5b17a2807dc8352a
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-12-14 15:28:06 -0800

    Un-Chest Demo
    Redoes the code and output file from before the Chest data export in the previous commit.
    
    I also adjusted the better indentation for the parsing function.

--- test/index.ts
+++ test/index.ts
@@ -1,6 +1,5 @@
-import { writeFile } from "node:fs/promises";
 import { fileURLToPath } from "node:url";
-import { read, stringify } from "nbtify";
+import { read } from "nbtify";
 import { readDatabase } from "../src/index.js";
 
 import type { RootTag, ReadOptions } from "nbtify";
@@ -20,40 +19,30 @@ const blockEntityActors: Buffer[] = data.chunks
   .map(chunk => chunk.BlockEntity)
   .filter((blockEntity): blockEntity is Buffer => blockEntity !== undefined);
 
-// const blockEntities: RootTag[][] = await Promise.all(
-//   blockEntityActors
-//     // .map(blockEntity => blockEntity.join(" "))
-//     .map(blockEntity => parseBlockEntityActor(blockEntity))
-// );
-// console.log(blockEntities);
-
-const demo: Buffer = blockEntityActors[2]!;
-console.log(demo);
-
-const blockEntities: string = (await parseBlockEntityActor(blockEntityActors[2]!))
-  .map(root => stringify(root,{ space: 2 }))
-  .join(",\n\n");
+const blockEntities: RootTag[][] = await Promise.all(
+  blockEntityActors
+    // .map(blockEntity => blockEntity.join(" "))
+    .map(blockEntity => parseBlockEntityActor(blockEntity))
+);
 console.log(blockEntities);
 
-await writeFile(new URL("./BlockEntity.bin",import.meta.url),blockEntities);
-
 export async function parseBlockEntityActor(blockEntity: Uint8Array): Promise<RootTag[]> {
-      const blockEntities: RootTag[] = [];
-      let byteOffsets: number[] = [];
-      let trailingBytes: Uint8Array;
+  const blockEntities: RootTag[] = [];
+  let byteOffsets: number[] = [];
+  let trailingBytes: Uint8Array;
 
-      for (let i = 0; i < blockEntity.byteLength; i++){
-        try {
-          const { data } = await read(blockEntity.subarray(i),{ ...FORMAT, strict: false } satisfies Required<ReadOptions>);
-          blockEntities.push(data);
-          byteOffsets.push(i);
-        } catch {}
-      }
-      trailingBytes = blockEntity.subarray(byteOffsets.at(-1) ?? 0);
+  for (let i = 0; i < blockEntity.byteLength; i++){
+    try {
+      const { data } = await read(blockEntity.subarray(i),{ ...FORMAT, strict: false } satisfies Required<ReadOptions>);
+      blockEntities.push(data);
+      byteOffsets.push(i);
+    } catch {}
+  }
+  trailingBytes = blockEntity.subarray(byteOffsets.at(-1) ?? 0);
 
-      console.log(byteOffsets);
-      console.log(trailingBytes
-        // .join(" ")
-      );
-      return blockEntities;
+  console.log(byteOffsets);
+  console.log(trailingBytes
+    // .join(" ")
+  );
+  return blockEntities;
 }
