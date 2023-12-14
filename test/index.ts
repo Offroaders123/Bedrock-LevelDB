a013efab668c3df64dc13da08c8068014ecf5d10
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-12-13 16:03:24 -0800

    Block Entity Actor Trailing Data
    Doesn't appear to be NBT, so I think we're onto something!
    
    So it's a group of NBT compound tags for an arbitrary length (I'm not saying List because it's not a List tag, to make that distinction), then some trailing data I'm yet to figure out.
    
    The console is logging the outputs/trailing data for 3 actors.
    (Actors are LevelDB entries, to clarify; trying to get used to MC's own terminology)
    
    The demo files newly added to `./test` are actor #2 from the console, parsed out/saved locally.

--- test/index.ts
+++ test/index.ts
@@ -2,7 +2,7 @@ import { fileURLToPath } from "node:url";
 import { read } from "nbtify";
 import { readDatabase } from "../src/index.js";
 
-import type { NBTData } from "nbtify";
+import type { RootTag } from "nbtify";
 
 const WORLD = fileURLToPath(new URL("../test/My World/db",import.meta.url));
 
@@ -11,12 +11,28 @@ const blockEntityActors: Buffer[] = data.chunks
   .map(chunk => chunk.BlockEntity)
   .filter((blockEntity): blockEntity is Buffer => blockEntity !== undefined);
 
-const blockEntities: NBTData[][] = await Promise.all(
+const blockEntities: RootTag[][] = await Promise.all(
   blockEntityActors
     // .map(blockEntity => blockEntity.join(" "))
-    .map(async (blockEntity): Promise<NBTData[]> => {
-      const { data } = await read(blockEntity,{ endian: "little", strict: false });
-      return data;
+    .map(async (blockEntity): Promise<RootTag[]> => {
+      const blockEntities: RootTag[] = [];
+      let byteOffsets: number[] = [];
+      let trailingBytes: Buffer;
+
+      for (let i = 0; i < blockEntity.byteLength; i++){
+        try {
+          const { data } = await read(blockEntity.subarray(i),{ name: "", endian: "little", compression: null, bedrockLevel: null, strict: false });
+          blockEntities.push(data);
+          byteOffsets.push(i);
+        } catch {}
+      }
+      trailingBytes = blockEntity.subarray(byteOffsets.at(-1) ?? 0);
+
+      console.log(byteOffsets);
+      console.log(trailingBytes
+        // .join(" ")
+      );
+      return blockEntities;
     })
 );
 console.log(blockEntities);
