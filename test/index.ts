d7c112ddabb7379b66dde649e0be0026d102cc30
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-12-14 21:18:41 -0800

    Format Loop Code
    This is really nice now!
    
    Here's an explanation I wrote about how the function goes through the buffer:
    
    The control flow is that it tried parsing the file outright, asserting that it read to the end of the file
    If that fails, then it gets the error byte offset from the error message (I am going to make better DX for this from NBTify, in relation to strict mode, this should be accessible as a simple property when parsing a file), then it reads from the current position until it completes, not asserting the end length of the file (non-strict mode), then it trims the view to exclude this read portion of the buffer
    
    So I think the takeaway from this is that I can better expose NBTify's use of strict mode through the NBTData object.
    
    https://github.com/Offroaders123/NBTify/issues/31

--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,6 @@
-import { readFile } from "node:fs/promises";
-// import { fileURLToPath } from "node:url";
-import { read, stringify } from "nbtify";
-// import { readDatabase } from "../src/index.js";
+import { fileURLToPath } from "node:url";
+import { read } from "nbtify";
+import { readDatabase } from "../src/index.js";
 
 import type { RootTag, ReadOptions } from "nbtify";
 
@@ -13,44 +12,30 @@ const FORMAT: Required<ReadOptions> = {
   strict: true
 };
 
-// const WORLD = fileURLToPath(new URL("../test/My World/db",import.meta.url));
+const WORLD = fileURLToPath(new URL("../test/My World/db",import.meta.url));
 
-// const data = await readDatabase(WORLD);
-// const blockEntityActors: Buffer[] = data.chunks
-//   .map(chunk => chunk.BlockEntity)
-//   .filter((blockEntity): blockEntity is Buffer => blockEntity !== undefined);
+const data = await readDatabase(WORLD);
+const blockEntityActors: Buffer[] = data.chunks
+  .map(chunk => chunk.BlockEntity)
+  .filter((blockEntity): blockEntity is Buffer => blockEntity !== undefined);
 
-const DEMO = new URL("./BlockEntity.bin",import.meta.url);
-
-const demo: Buffer =
-  // blockEntityActors[2]!;
-  await readFile(DEMO);
-
-const ids: string[] = demo.toString("utf-8").split("id").map(id => id.split("\x01")?.[0]?.split("\x00")[1]?.replace(/\W/g,"") ?? "").filter(string => /^[A-Z]/g.test(string));
-console.log(ids);
-
-const blockEntities: RootTag[] = await parseBlockEntityActor(demo);
-console.log(blockEntities
-  .filter(blockEntity => "id" in blockEntity)
-  // .map(blockEntity => stringify(blockEntity,{ space: 2 }))
-  // .join(",\n\n")
+const blockEntities: RootTag[][] = await Promise.all(
+  blockEntityActors
+    .map(blockEntity => parseBlockEntityActor(blockEntity))
 );
+console.log(blockEntities);
 
 export async function parseBlockEntityActor(data: Uint8Array): Promise<RootTag[]> {
   const blockEntities: RootTag[] = [];
 
-  // for (let i = 0; i < 9; i++){
   while (true){
     try {
       const blockEntity: RootTag = await read(data,FORMAT).then(data => data.data);
       blockEntities.push(blockEntity);
-      console.log("end!");
       break;
     } catch (error){
       const message: string = (error as Error).message ?? `${error}`;
-      console.log(message);
       const length: number = parseInt(message.slice(46));
-      console.log(length);
       const blockEntity: RootTag = await read(data,{ ...FORMAT, strict: false }).then(data => data.data);
       blockEntities.push(blockEntity);
       data = data.subarray(length);
