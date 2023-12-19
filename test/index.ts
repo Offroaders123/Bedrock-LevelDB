eca1f43de267aba1f3992115e18b014ef5baabb1
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-12-18 19:52:15 -0800

    World Folder
    Simpler diff resetting when demoing opening the databases from the test folder, now I can just run `git clean -df ./test/world/; git checkout ./test/world/` instead of having to do it for each world folder, when trying to parse each out to see what happens.

--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,7 @@
 import { fileURLToPath } from "node:url";
 import { readDatabase } from "../src/index.js";
 
-const WORLD = fileURLToPath(new URL("../test/My World/db",import.meta.url));
+const WORLD = fileURLToPath(new URL("../test/world/My World/db",import.meta.url));
 
 const data = await readDatabase(WORLD);
 console.log(data);
