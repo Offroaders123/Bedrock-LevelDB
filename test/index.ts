4ff7178fef209efd4a96a200db012048aca2093e
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-12-29 10:40:24 -0800

    Dictionary / DigP Experimentation
    Trying to figure out what the IDs for the dimensions correlate to, I thought maybe they had to do with the IDs for DigP entries? Looks like that's not the case though, those aren't used for this.

--- test/index.ts
+++ test/index.ts
@@ -4,8 +4,20 @@ import { LevelChunkMetaDataDictionary, readDatabase } from "../src/index.js";
 const WORLD = fileURLToPath(new URL("../test/world/dOCMZTQ2AAA=/db",import.meta.url));
 
 const data = await readDatabase(WORLD);
-console.log(data.LevelChunkMetaDataDictionary);
 
-for (const entry of (data.LevelChunkMetaDataDictionary as LevelChunkMetaDataDictionary).entries){
-  console.log(entry.tag.data);
+// Object.keys(data).forEach(key => console.log(key));
+
+const dictionaryKeys: string[] = (data.LevelChunkMetaDataDictionary as LevelChunkMetaDataDictionary).entries
+  .map(entry => entry.key.toString());
+console.log(dictionaryKeys);
+
+for (const key of Object.entries(data)
+  .filter(entry => entry[0].match(/^digp/))
+  .map(entry => entry[0])
+){
+  // console.log(key);
+  for (const dictionaryKey of dictionaryKeys){
+    if (!key.includes(dictionaryKey)) continue;
+    console.log(key);
+  }
 }
