2edb1716770cf4fc32a78e5f2c43661f427df7d5
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2023-12-29 13:29:23 -0800

    Dictionary Debug (Not string, long, or UUID!)
    Tried reading the bytes as various formats, doesn't seem to be valid in any of the ones I tested.
    
    Alongside the code here, I also tried parsing the buffers as UUIDs with `uuid.stringify()`, but they threw saying the result UUIDs were invalid.
    
    https://www.npmjs.com/package/uuid#api-summary
    https://www.npmjs.com/package/uuid#uuidstringifyarr-offset

--- test/index.ts
+++ test/index.ts
@@ -7,17 +7,27 @@ const data = await readDatabase(WORLD);
 
 // Object.keys(data).forEach(key => console.log(key));
 
-const dictionaryKeys: string[] = (data.LevelChunkMetaDataDictionary as LevelChunkMetaDataDictionary).entries
-  .map(entry => entry.key.toString());
+const dictionaryKeys = (data.LevelChunkMetaDataDictionary as LevelChunkMetaDataDictionary).entries
+  .map(entry => [
+    entry.key,
+    entry.key.join(" "),
+    entry.key.toString("utf-8"),
+    entry.key.toString("ascii"),
+    entry.key.toString("utf-16le"),
+    entry.key.readBigInt64BE(0),
+    entry.key.readBigInt64LE(0),
+    entry.key.readBigUInt64BE(0),
+    entry.key.readBigUInt64LE(0)
+  ]);
 console.log(dictionaryKeys);
 
-for (const key of Object.entries(data)
-  .filter(entry => entry[0].match(/^digp/))
-  .map(entry => entry[0])
-){
-  // console.log(key);
-  for (const dictionaryKey of dictionaryKeys){
-    if (!key.includes(dictionaryKey)) continue;
-    console.log(key);
-  }
-}
+// for (const key of Object.entries(data)
+//   .filter(entry => entry[0].match(/^digp/))
+//   .map(entry => entry[0])
+// ){
+//   // console.log(key);
+//   for (const dictionaryKey of dictionaryKeys){
+//     if (!key.includes(dictionaryKey)) continue;
+//     console.log(key);
+//   }
+// }
